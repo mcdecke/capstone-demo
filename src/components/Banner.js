3e4d4ef50bb306d41b25caf8cffd1e65560d789e
Mac Decker <mcdecke@gmail.com>
2018-07-24 11:14:21 -0600

    cleaned up

---
+++ src/components/Banner.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+// import '../index.css';
+// import '../App.css';
+import Password from './Password'
+// import App from '../App'
+
+class Banner extends Component {
+
+render() {
+    return (
+      <div>
+        Hi
+      </div>
+    )
+  }
+}
+
+export default Banner
